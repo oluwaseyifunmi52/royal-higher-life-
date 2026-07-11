f4349c261234aad7a1cb3505b60552bef901288a
unknown <oluwaseyifunmioluwadami@email.com>
2026-07-11 18:28:28 +0100

    higherlife

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    
-    baseURL: "https://royal-higher-life-backend-2o66.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
 });
 
-
-// Attach JWT token automatically to every request
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 });
 
